d0934e8360ccaa70b422556c5a552ebd2b6c48dc
Brian Jenney <brianjenney@MacBook-Pro.local>
2025-04-28 16:44:46 -0700

    fix remote patterns

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-		remotePatterns: [new URL('https://i.scdn.co/**')],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'i.scdn.co',
+			},
+		],
 	},
 };
 
